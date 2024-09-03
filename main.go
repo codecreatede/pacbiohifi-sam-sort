8347362e30343983b10a90255175f22bfaaaa0df
Gaurav Sablok <codeprog@icloud.com>
2024-09-03 23:20:27 +0200

    codecreatede
    bug fix in the struct and the method called and now struct and the readBuffer and the readSam are properly taking and feeding into the struct the required parameter for the SQL

--- main.go
+++ main.go
@@ -1,69 +1,74 @@
-package main 
+package main
 
 /*
 Author Gaurav Sablok
 Universitat Potsdam
 Date 2024-8-27
 
-structs and inheritances for making the pacbiohifi genome alignments easier
-for the web browse and calling through the http. It takes the alignment files,
-parses the alignments and then makes a struct based JSON for easy feed into the
-database and also writes a CSV for direct import into the SQLITE, MongoDB and PostGresSQL.
+structs and inheritances for making the pacbiohifi genome alignments easier for the web browse and calling through the http. It takes the sam alignment files, parses the alignments and then makes a struct based JSON for easy feed into the database and also writes a CSV for direct import into the SQLITE, MongoDB and PostGresSQL.
 
 */
 
 import (
-   "bufio"
-   "fmt"
-   "log"
-   "os"
-   "strings"
+	"bufio"
+	"log"
+	"os"
+	"strings"
 )
 
+func main() {
+	// struct for the sam file define
 
-func main () {
+	type readStore struct {
+		idread string
+		tag    string
+		seq    string
+	}
 
-type readStore struct {
-	idread string
-	tag int
-	seq string
-}
-
-
-readFile := os.Args[1:]
-writefile := os.Args[2:]
-if len(readFile) == nil {
-	panic(err)
-	log.Fatal(err)
-} else {
+	// reading the fileIO
 	openFile, err := os.Open("readFile")
 	if err != nil {
-		panic(err)
 		log.Fatal(err)
 	}
+
+	// bufferscan and feeding into the struct
 	readBuffer := bufio.NewScanner(openFile)
 	for readBuffer.Scan() {
 		line := readBuffer.Text()
 		if strings.HasPrefix(line, "@") {
 			continue
 		} else {
-			storeRead := []readStore{}
-			storeRead = append(storeRead, readStore{idread: strings.Split(line, "\t")[0],
-				    tag: strings.Split(line, "\t")[5],
-				    seq: strings.Split(line,"\t")[9],
-			}
+			data := []readStore{}
+			data = append(data, readStore{
+				idread: strings.Split(line, "\t")[0],
+				tag:    strings.Split(line, "\t")[5],
+				seq:    strings.Split(line, "\t")[9],
+			})
 		}
 	}
-        create, err := os.createFile("writefile")
+	// writing the csv for direct ingestion into the SQL, SQLite3, POSTGresSQL
+	create, err := os.Create("writefile")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+	readSam := bufio.NewScanner(openFile)
+	for readSam.Scan() {
+		line := readSam.Text()
+		if strings.HasPrefix(line, "@") {
+			continue
+		} else {
+			data := []readStore{}
+			data = append(data, readStore{
+				idread: strings.Split(line, "\t")[0],
+				tag:    strings.Split(line, "\t")[5],
+				seq:    strings.Split(line, "\t")[9],
+			})
+		}
 	}
 	defer create.Close()
-	for i := range storeRead {
+	for i := range data {
 		create.WriteString("The mapped alignment reads along with the CIGAR values are:")
-		create.WriteString("%s, %s, %s", storeRead.idread, storeRead.tag, storeRead.seq)
+		create.WriteString("%s, %s, %s", data.idread, data.tag, data.seq)
 		create.WriteString("The file for the sql injection and transaction has been written")
 	}
-
-}
 }
